977dd7f71074a12e4a85d9056e762bf790dd58f5
5thGenDev <44685200+5thGenDev@users.noreply.github.com>
2023-12-22 12:59:35 +0000

    Update Day5Part2_main.py

--- Day5/Day5Part2_main.py
+++ Day5/Day5Part2_main.py
@@ -64,33 +64,13 @@ def dfs_iterative(start_node):
 
     while stack:
         node, parent = stack.pop(-1)
-        node_visited, visited = node_in_visited(visited, node, parent)
-        if not node_in_visited(visited, node, parent):
+        if not node_in_visited(visited, node):
             predecessor[node] = parent # add node : parent to dict
-            print(f"Visiting: {node}, Parent: {parent}")
+            visited.append(node)
+            #print(f"Visiting: {node}, Parent: {parent}")
 
             if node[3] == 6: # equivalent of <if goal is reached>
-                # Progagate universal src from final to start node (from seed -> location)
-                # whenever source of current_node > destination of child_node
-                # final result of universal src = virtual minimum seed 
-                propagation_path = []
-                current_node = node 
-                universal_src = current_node[1]
-                while current_node is not None:
-                    # notice how propagation_path only contains child_nodes
-                    propagation_path.append(current_node)
-                    child_node = current_node
-                    current_node = predecessor[current_node] # get value from dict()
-                    if current_node is not None:
-                        if current_node[1] > child_node[0]:
-                            src = universal_src
-                            for successor_node in propagation_path:
-                                # destination in child_node -> source in parent_node
-                                src = src + (successor_node[0] - successor_node[1])
-                            if current_node[1] > src:
-                                universal_src = universal_src + (current_node[1] - src)
-            
-                #print(f"Universal Source is : {universal_src}")
+                universal_src = find_universal_src(node, predecessor)
 
                 seedFound = False
                 for seed in seeds:
@@ -100,19 +80,17 @@ def dfs_iterative(start_node):
                         predecessor[seed] = node
                         target = seed
                         seedFound = True
-                        print("\n \nTarget reach")
+                        print(f"Universal Source is : {universal_src}")
                         break
                 if seedFound:
                     break
             else:
                 connected_nodes = get_connected_nodes(node)
-                if not connected_nodes:
-                    visited.append(node)
                 if connected_nodes:
-                    print(f"Connected nodes of {node}: {connected_nodes} \n")
+                    #print(f"Connected nodes of {node}: {connected_nodes} \n")
                     for next_node in connected_nodes:
-                        next_node_visited, visited = node_in_visited(visited, next_node, node)
-                        if not next_node_visited:
+                        visited = release_child_from_visited(visited, next_node)
+                        if not node_in_visited(visited, next_node):
                             stack.append(tuple([next_node, node]))
                     visited.append(node)
 
